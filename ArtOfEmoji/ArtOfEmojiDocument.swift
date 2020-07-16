532da3c2ab9e96c01a9ab19bd5584b9eb43f20e1
P-Jag <pavel.jagla@gmail.com>
2020-07-16 13:45:09 +0200

    initial commit

---
+++ ArtOfEmoji/ArtOfEmojiDocument.swift
@@ -0,0 +1,13 @@
+//
+//  ArtOfEmojiDocument.swift
+//  ArtOfEmoji
+//
+//  Created by Paweł Jagła on 16/07/2020.
+//  Copyright © 2020 Paweł Jagła. All rights reserved.
+//
+
+import SwiftUI
+
+class ArtOfEmojiDocument: ObservableObject {
+    
+}
---
+++ ArtOfEmoji/ArtOfEmojiDocumentView.swift
@@ -0,0 +1,17 @@
+//
+//  ArtOfEmojiDocumentView.swift
+//  ArtOfEmoji
+//
+//  Created by Paweł Jagła on 16/07/2020.
+//  Copyright © 2020 Paweł Jagła. All rights reserved.
+//
+
+import SwiftUI
+
+struct ArtOfEmojiDocumentView: View {
+    @ObservedObject var document: ArtOfEmojiDocument
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
